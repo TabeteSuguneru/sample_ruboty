beed0fa0dc1b2dec66748edef3e27e7bd568eb4b
Yuma Kimura <y.kimura.0608@gmail.com>
2021-05-16 20:41:39 +0900

    first commit

---
+++ hello.rb
@@ -0,0 +1,13 @@
+require "pry"
+
+module Ruboty
+  module Handlers
+    class Hello < Base
+      on(/hello/i, name:"hello", description: 'こんにちは')
+
+      def hello(message)
+        message.reply("Hello!!")
+      end
+    end
+  end
+end
